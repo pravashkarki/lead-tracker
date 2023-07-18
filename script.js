9f9d1cc02e515534e57060d0aeb48c217f7941a8
Pravash Karki <pravash.karki@heraldcollege.edu.np>
2023-07-18 16:33:22 +0545

    push the value form input field

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let buttonEL = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const buttonEL = document.getElementById("input-btn")
 
 buttonEL.addEventListener("click", function(){
-    alert("Button clicked!")
+    myLeads.push(inputEl.value)
 });
